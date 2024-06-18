af1501dc1a661953bca01f32fb873f6fef6faec7
deathvenom54 <devanshkandpal54@gmail.com>
2024-06-18 23:21:20 +0530

    First commit, just added rust and python

--- lua/community.lua
+++ lua/community.lua
@@ -8,5 +8,7 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
   -- import/override with your plugins folder
 }
